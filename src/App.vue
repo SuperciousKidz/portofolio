8d8b42a1a03205728e2732c0e559156bb4bfeede
andreantolis <andre.antolis@gdn-commerce.com>
2022-01-28 13:53:40 +0700

    adding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <home></home>
+    <skills></skills>
+    <projects></projects>
+    <about></about>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import home from "./components/Home.vue";
+import skills from "./components/Skills.vue"
+import projects from "./components/Projects.vue"
+import about from "./components/About.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'home': home,
+    'skills': skills,
+    'projects': projects,
+    'about': about
   }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
